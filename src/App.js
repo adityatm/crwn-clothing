44810789ec49987c1b5e11a1e2fa606156df6fc6
Boon <admin@Admins-MacBook-Air.local>
2020-07-13 00:15:22 +0530

    collection preview and collection items component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component';
 
 function HatsPage(){
   return(
@@ -17,7 +18,7 @@ function App() {
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route exact path="/shop/hats" component={HatsPage} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
