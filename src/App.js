878a6ef76f098abc28bb6c605609e120b089ad9b
Boon <admin@Admins-MacBook-Air.local>
2020-07-12 23:15:49 +0530

    routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+
+function HatsPage(){
+  return(
+    <div>
+      <h1>Hats Page</h1>
+    </div>
+  )
+}
+
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
+
+
 
 export default App;
