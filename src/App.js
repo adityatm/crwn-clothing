3998ac3cfedfb7260bdcb616105d957d42b28a24
Boon <admin@Admins-MacBook-Air.local>
2020-07-13 00:32:45 +0530

    header

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
 import './App.css';
+
 import { Route, Switch } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
-function HatsPage(){
-  return(
-    <div>
-      <h1>Hats Page</h1>
-    </div>
-  )
-}
+
+// function HatsPage(){
+//   return(
+//     <div>
+//       <h1>Hats Page</h1>
+//     </div>
+//   )
+// }
 
 
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
