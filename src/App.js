e5bb74e5955ef6865620ac2be03e547edcbe8933
Boon <admin@Admins-MacBook-Air.local>
2020-07-13 17:23:45 +0530

    sign up authentication

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { Route, Switch } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import {auth} from '../src/firebase/firebase.utils';
+import {auth, createUserProfileDocument} from '../src/firebase/firebase.utils';
 
 
 class App extends React.Component {
@@ -22,9 +22,21 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount(){
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user =>{
-      this.setState({currentUser: user})
-      console.log(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth =>{
+      if(userAuth){
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapshot =>{
+          this.setState({
+            currentUser:{
+              id: snapshot.id,
+              ...snapshot.data()
+            }
+          })
+          console.log(this.state);
+        })
+      }
+      this.setState({currentUser: userAuth});
     })
   }
 
